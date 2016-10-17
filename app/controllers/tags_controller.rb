ee782662f9477a1725229f61a3d831ebc9365f83
Sal Espinosa <sespinos@gmail.com>
2016-10-16 21:02:04 -0600

    Complete tutorial

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,16 @@
+class TagsController < ApplicationController
+  before_filter :require_login, only: [:destroy]
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    Tag.find(params[:id]).destroy
+    redirect_to tags_path
+  end
+end
